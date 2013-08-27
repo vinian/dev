423da52c2cd84c264efaa2a0e66f8e15175e7962
VINIAN <willian.wnh@gmail.com>
2013-08-27 15:16:39 +0800

    get text from epub

---
+++ extract_text_from_epub.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use 5.014;
+
+use HTML::TagParser;
+use File::Temp qw(tempdir);
+
+my ($file, $text_file) = @ARGV;
+if (not defined $text_file or $file !~ /epub$/) {
+    say "Usage: $0 file.epub output";
+    exit 1;
+}
+
+if (not -e $file) {
+    say "file: $file not existed.";
+    exit 2;
+}
+
+my $tmp_dir = tempdir( CLEANUP => 1);
+`unzip $file -d $tmp_dir`;
+my @htmls = glob("$tmp_dir/OEBPS/Text/*xhtml");
+
+
+open my $fh, '>', $text_file
+    or die "Can't open $text_file: $!";
+for my $page (
+    sort {
+        my ($a_num) = $a =~ /s(\d+)\.xhtml/;
+        my ($b_num) = $b =~ /s(\d+)\.xhtml/;
+        $a_num <=> $b_num;
+    } @htmls) {
+
+    my $html = HTML::TagParser->new( $page );
+    for my $div ( $html->getElementsByTagName('div') ) {
+        my $text = $div->innerText;
+        print $fh $text;
+    }
+}
+
+close $fh;
